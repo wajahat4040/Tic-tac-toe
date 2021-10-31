396093fcff65f64c3c95bbd118cb65d475861044
wajahat4040 <wajahatkhan4040@gmail.com>
2021-10-31 19:33:26 +0500

    initial commit

---
+++ components/Cross.js
@@ -0,0 +1,59 @@
+/**
+ * Sample React Native App
+ *
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Cross extends Component {
+  render() {
+    const { xTranslate, yTranslate, color } = this.props;
+    return (
+      <View
+        style={[
+          styles.container,
+          {
+            transform: [
+              { translateX: (xTranslate ? xTranslate : 10) + 35 },
+              { translateY: (yTranslate ? yTranslate : 10) - 12 },
+            ],
+          },
+        ]}
+      >
+        <View
+          style={[
+            styles.line,
+            {
+              transform: [{ rotate: '45deg' }],
+              backgroundColor: color ? color : '#000',
+            },
+          ]}
+        />
+        <View
+          style={[
+            styles.line,
+            {
+              transform: [{ rotate: '135deg' }],
+              backgroundColor: color ? color : '#000',
+            },
+          ]}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    width: 80,
+    height: 80,
+  },
+  line: {
+    position: 'absolute',
+    width: 8,
+    height: 105,
+  },
+});
